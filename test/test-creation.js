a785785223bcf5cb01da68abd340f17b44a8115c
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2014-12-27 02:49:36 -0200

    Improve tests

--- test/test-creation.js
+++ test/test-creation.js
@@ -10,10 +10,22 @@ describe('python-package generator', function () {
         return done(err);
       }
 
-      this.app = helpers.createGenerator('python-package:app', [
-        '../../app'
-      ]);
+      this.app = helpers.createGenerator('python-package:app', ['../../app']);
       done();
+
+      this.app.options['skip-install'] = true;
+
+      helpers.mockPrompt(this.app, {
+        packageName: 'test-package',
+        pythonVersions: ['2.7@py27@Python :: 2.7'],
+        description: 'an incredible python package',
+        keywords: 'test package',
+        authorName: 'Pablo Santiago Blum de Aguiar',
+        authorEmail: 'scorphus@gmail.com',
+        url: 'https://github.com/someuser/somepackage',
+        license: 'MIT',
+        services: ['mongodb', 'redis'],
+      });
     }.bind(this));
   });
 
@@ -35,15 +47,47 @@ describe('python-package generator', function () {
       'tests/test_version.py',
     ];
 
-    helpers.mockPrompt(this.app, {
-      packageName: 'test-package',
-      pythonVersions: ['2.7@py27@Python :: 2.7'],
-      services: ['mongodb', 'redis'],
-    });
-
-    this.app.options['skip-install'] = true;
     this.app.run({}, function () {
       helpers.assertFile(expected);
+      done();
+    });
+  });
+
+  it('creates Makefile with right targets files', function (done) {
+    this.app.run({}, function () {
+      helpers.assertFileContent('Makefile', /list:/);
+      helpers.assertFileContent('Makefile', /no_targets__:/);
+      helpers.assertFileContent('Makefile', /setup:/);
+      helpers.assertFileContent('Makefile', /test:/);
+      helpers.assertFileContent('Makefile', /unit:/);
+      helpers.assertFileContent('Makefile', /redis:/);
+      helpers.assertFileContent('Makefile', /kill_redis:/);
+      helpers.assertFileContent('Makefile', /redis_test:/);
+      helpers.assertFileContent('Makefile', /kill_redis_test:/);
+      helpers.assertFileContent('Makefile', /mongo:/);
+      helpers.assertFileContent('Makefile', /kill_mongo:/);
+      helpers.assertFileContent('Makefile', /clear_mongo:/);
+      helpers.assertFileContent('Makefile', /mongo_test:/);
+      helpers.assertFileContent('Makefile', /kill_mongo_test:/);
+      helpers.assertFileContent('Makefile', /tox:/);
+      done();
+    });
+  });
+
+  it('creates setup.py with right elements', function (done) {
+    this.app.run({}, function () {
+      helpers.assertFileContent('setup.py', /from test_package import __version__/);
+      helpers.assertFileContent('setup.py', /name='test-package',/);
+      helpers.assertFileContent('setup.py', /    version=__version__,/);
+      helpers.assertFileContent('setup.py', /    description='an incredible python package',/);
+      helpers.assertFileContent('setup.py', /\nan incredible python package\n/);
+      helpers.assertFileContent('setup.py', /    keywords='test package',/);
+      helpers.assertFileContent('setup.py', /    author='Pablo Santiago Blum de Aguiar',/);
+      helpers.assertFileContent('setup.py', /    author_email='scorphus@gmail.com',/);
+      helpers.assertFileContent('setup.py', /    url='https:\/\/github.com\/someuser\/somepackage',/);
+      helpers.assertFileContent('setup.py', /    license='MIT',/);
+      helpers.assertFileContent('setup.py', /        'Programming Language :: Python :: 2.7',/);
+      helpers.assertFileContent('setup.py', /    include_package_data=False,/);
       done();
     });
   });
