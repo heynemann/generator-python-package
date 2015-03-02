aeeb55367bccfc84c99578f5401e80f8c50aee15
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-03-01 23:16:15 -0200

    Improve comment

--- app/templates/_setup.py
+++ app/templates/_setup.py
@@ -48,7 +48,7 @@ setup(
     include_package_data=<%= package.includePackageData ? "True" : "False" %>,
     install_requires=[
         # add your dependencies here
-        # remember to use 'package-name>=x.y.z,<x.y+1.0' notation (this way you get bugfixes)
+        # remember to use 'package-name>=x.y.z,<x.(y+1).0' notation (this way you get bugfixes but no breaking changes)
     ],
     extras_require={
         'tests': tests_require,
