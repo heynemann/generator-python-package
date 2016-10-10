1ecbce43c9e0fa5a0d85818e1e6be8d7147427ba
kxxoling <kxxoling@gmail.com>
2016-10-10 14:50:39 +0800

    Remove license declarations in templates; changing coding declaration style.

--- app/templates/_setup.py
+++ app/templates/_setup.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# This file is part of <%= package.name %>.
-# <%= package.url %>
-
-# Licensed under the <%= package.license %> license:
-# http://www.opensource.org/licenses/<%= package.license%>-license
-# Copyright (c) <%= package.created.year %>, <%= package.author.name %> <<%= package.author.email %>>
-
+# coding: utf-8
 from setuptools import setup, find_packages
+
 from <%= package.pythonName %> import __version__
+
 
 tests_require = [
     'mock',
--- app/templates/_version.py
+++ app/templates/_version.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# This file is part of <%= package.name %>.
-# <%= package.url %>
-
-# Licensed under the <%= package.license %> license:
-# http://www.opensource.org/licenses/<%= package.license%>-license
-# Copyright (c) <%= package.created.year %>, <%= package.author.name %> <<%= package.author.email %>>
+# coding: utf-8
 
 __version__ = '<%= package.version %>'  # NOQA
