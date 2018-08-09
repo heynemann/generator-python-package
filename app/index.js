90351235fcceb606f4ace8fab626644d97e78b0b
Paul Craciunoiu <paul@craciunoiu.net>
2018-08-09 11:49:03 -0600

    Update available python versions

--- app/index.js
+++ app/index.js
@@ -56,12 +56,13 @@ var PythonPackageGenerator = yeoman.generators.Base.extend({
     }
 
     var pythonVersions = [
-      { name: "Python 2.6", value: "2.6@py26@Python :: 2.6", checked: false },
       { name: "Python 2.7", value: "2.7@py27@Python :: 2.7", checked: true },
       { name: "Python 3.2", value: "3.2@py32@Python :: 3.2", checked: false },
       { name: "Python 3.3", value: "3.3@py33@Python :: 3.3", checked: false },
-      { name: "Python 3.4", value: "3.4@py34@Python :: 3.4", checked: true },
+      { name: "Python 3.4", value: "3.4@py34@Python :: 3.4", checked: false },
       { name: "Python 3.5", value: "3.5@py35@Python :: 3.5", checked: true },
+      { name: "Python 3.6", value: "3.6@py36@Python :: 3.6", checked: true },
+      { name: "Python 3.7", value: "3.7@py37@Python :: 3.7", checked: true },
       { name: "PyPy", value: "pypy@pypy@Python :: Implementation :: PyPy", checked: true },
     ];
 
